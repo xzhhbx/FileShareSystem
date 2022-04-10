1b7f33b74329ab8f3da6fd3c24aa53b10941c413
落雨sa <69842604+MoeLuoYu@users.noreply.github.com>
2022-04-10 23:32:30 +0800

    语言文件

---
+++ lang/en_us.php
@@ -0,0 +1,46 @@
+<?php
+/*
+ Author:AroRain(MoeLuoYu)
+ This is free software,but you can use it for business.
+ $ id: FileShareSystem_lang_en_us 2022-4-10 CST MoeLuoYu $
+ This translations made by MoeLuoYu
+*/
+
+// index.php
+define("notperm", "You are not authorized to access the current directory");
+define("no", "This folder is empty.");
+define("disksize", "Disk Capacity");
+define("freesize", "Available Space");
+define("up", "Back to the top");
+define("viewdir", "View Catalog");
+define("cancel", "Cancel");
+define("all", "Select All");
+define("dirlist", "Catalog List");
+define("viewfile", "View");
+define("download", "Download");
+// template.php
+define("cop", "All Right Reserved");
+// 404.php
+define("notfound", "Not Found");
+// view.php
+define("view", "View File");
+define("backdir", "Back to Catalog");
+define("errtip", "Error Tip");
+define("noreadperm", "You do not have read access to this file!");
+define("code", "Encode");
+define("nom", "Missing modules");
+define("reopen", "Reopen by code");
+define("interview", "Content Browse");
+define("big", "The file is too big, please download it and check it out!");
+define("nullpoint", "The file cannot be opened or the file content is empty!");
+//file.php
+define("fileinfo", "File Info");
+define("filename", "File Name");
+define("sizefile", "File Size");
+define("mime", "File Type");
+define("latest", "Last visit");
+define("latestedit", "Final Edit");
+define("md5hash", "File Validation");
+define("md5value", "MD5 hash of the file");
+define("sha1value", "SHA1 hash of the file");
+?>
---
+++ lang/zh_cn.php
@@ -0,0 +1,46 @@
+<?php
+/*
+ Author:AroRain(MoeLuoYu)
+ This is free software,but you can use it for business.
+ $ id: FileShareSystem_lang_zh_cn 2022-4-10 CST MoeLuoYu $
+ This translations made by MoeLuoYu
+*/
+
+// index.php
+define("notperm", "您无权访问当前目录");
+define("no", "此文件夹为空。");
+define("disksize", "磁盘容量");
+define("freesize", "可用空间");
+define("up", "返回上级");
+define("viewdir", "查看目录");
+define("cancel", "取消");
+define("all", "全选");
+define("dirlist", "目录列表");
+define("viewfile", "查看");
+define("download", "下载");
+// template.php
+define("cop", "版权所有");
+// 404.php
+define("notfound", "你的请求资源无法找到");
+// view.php
+define("view", "查看文件");
+define("backdir", "返回目录");
+define("errtip", "错误提示");
+define("noreadperm", "此文件您没有读取权限！");
+define("code", "编码");
+define("nom", "缺少模块");
+define("reopen", "通过编码重新打开");
+define("interview", "内容浏览");
+define("big", "文件过大，请下载后查看！");
+define("nullpoint", "文件无法打开或文件内容为空！");
+//file.php
+define("fileinfo", "文件信息");
+define("filename", "文件名称");
+define("sizefile", "文件大小");
+define("mime", "文件类型");
+define("latest", "最后访问");
+define("latestedit", "最后编辑");
+define("md5hash", "文件校验");
+define("md5value", "文件的MD5散列值");
+define("sha1value", "文件的SHA1散列值");
+?>
